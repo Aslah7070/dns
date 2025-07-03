bf11d4299f693556927b2eaf3092c1e6706f2d82
Aslah7070 <aslah.c7070@gmail.com>
2025-07-03 09:30:09 +0530

    check axios instance

--- front_end/src/lib/services/api.ts
+++ front_end/src/lib/services/api.ts
@@ -1,6 +1,7 @@
 import { env } from "../../configs/env.config";
 import axios from "axios";
 // import Cookies from "js-cookie";
+console.log("env.BACKEND_URI",env.BACKEND_URI);
 
 const axiosInstance = axios.create({
     baseURL:env.BACKEND_URI, 
